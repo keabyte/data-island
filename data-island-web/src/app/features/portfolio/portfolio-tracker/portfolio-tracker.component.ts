6a7b137a7e3982c8cfb988331a78028a1890da0c
Taylar <keast.taylar@gmail.com>
2023-01-16 21:34:44 +1100

    Create portfolio

--- data-island-web/src/app/features/portfolio/portfolio-tracker/portfolio-tracker.component.ts
+++ data-island-web/src/app/features/portfolio/portfolio-tracker/portfolio-tracker.component.ts
@@ -26,6 +26,22 @@ export class PortfolioTrackerComponent implements OnInit {
 
 	deletePortfolio(portfolio: Portfolio) {
 		this.portfolios = this.portfolios.filter(p => p.order !== portfolio.order);
-		this.selectPortfolio(this.portfolios[0]);
+		this.selectPortfolio(this.portfolios.reverse().find(p => p.order < portfolio.order));
+		this.portfolios.reverse();
+	}
+
+	createPortfolio() {
+		const maxOrderAssetRow = this.portfolios.sort((a, b) => (a.order > b.order ? -1 : 1))[0];
+
+		const portfolio: Portfolio = {
+			name: 'New portfolio',
+			assets: [],
+			createdDate: new Date(),
+			totalValue: 0,
+			order: (maxOrderAssetRow?.order || 0) + 1
+		};
+		this.portfolios.push(portfolio);
+		this.portfolios = this.portfolios.sort((a, b) => (a.order > b.order ? 1 : -1));
+		this.selectPortfolio(portfolio);
 	}
 }
