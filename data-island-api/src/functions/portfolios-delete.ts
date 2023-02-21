093cc179924fefd0a769ae036bb964dd0d71723c
Taylar <keast.taylar@gmail.com>
2023-02-21 22:25:10 +1100

    Delete portfolio API

---
+++ data-island-api/src/functions/portfolios-delete.ts
@@ -0,0 +1,35 @@
+import { DeleteItemCommand, DynamoDB } from '@aws-sdk/client-dynamodb';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { Request } from '../util/request';
+import { Table } from '../util/table';
+
+module.exports.handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const request = new Request(event);
+	const dynamodb = new DynamoDB({
+		region: 'localhost',
+		endpoint: 'http://localhost:8000'
+	});
+
+	const { portfolioId } = request.pathParameters;
+
+	const params = {
+		TableName: Table.PORTFOLIO,
+		Key: {
+			id: { S: portfolioId }
+		}
+	};
+
+	try {
+		await dynamodb.send(new DeleteItemCommand(params));
+		return {
+			statusCode: 200,
+			body: JSON.stringify(`Deleted portfolio ${portfolioId}`)
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ message: `Could not delete porfolio ${portfolioId}` })
+		};
+	}
+};
--- data-island-api/src/util/request.ts
+++ data-island-api/src/util/request.ts
@@ -2,12 +2,18 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 
 export class Request {
 	private _body: any;
+	private _pathParameters: any;
 
 	constructor(event: APIGatewayProxyEvent) {
 		this._body = event.body ? JSON.parse(event.body) : {};
+		this._pathParameters = event.pathParameters;
 	}
 
 	get body() {
 		return this._body;
+	}
+
+	get pathParameters() {
+		return this._pathParameters;
 	}
 }
